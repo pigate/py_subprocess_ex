d58bc7a50c89aaf311e6bf5007117d72178efcea
pigate <mimchiang@gmail.com>
2015-09-26 16:58:01 -0700

    created misubprocess

---
+++ misubprocess.py
@@ -0,0 +1,20 @@
+import sys
+import os
+
+class MiniMe:
+  def __init__(self):
+    print "Implement Me"
+
+  def __init__(self, lambda, done):
+    print "Implement Me Please"
+
+  def start():
+    print "Start to implement me please"
+
+  def signal(sig):
+    print "Signal, implement me"
+
+  def kill():
+    print "Kill me, implement me" 
+ 
+  
