40015d54c6bf20af44e4a2eb8c2f79ab7c190235
pigate <mimchiang@gmail.com>
2015-09-26 16:00:35 -0700

    edited run.py to call other.py

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ import subprocess
 can have processes communicate with each other via pipes
 """
 
-subprocess.call(['ls', '-l'], shell=True)
-subprocess.call('echo $HOME', shell=True)
-subprocess.call('echo $$ $BASHPID', shell=True)
-exit_code = subprocess.call('echo $$', shell=True)
+#subprocess.call(['ls', '-l'], shell=True)
+#subprocess.call('echo $HOME', shell=True)
+#subprocess.call('echo $$ $BASHPID', shell=True)
+#exit_code = subprocess.call('echo $$', shell=True)
+subprocess.call(['python', 'other.py'])
